ba6b9ca8398a0ddaeb1cc9b3a763565e2adb6ae9
Evaldas Visockas <vievaldas@gmail.com>
2023-12-29 19:25:11 +0200

    Add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import NextAuthProvider from "./NextAuthProvider"
 import { Metadata } from "next"
 import React from "react"
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: 'Programmers diary',
@@ -12,16 +13,24 @@ export const metadata: Metadata = {
 }
 
 export default async function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    const session = await getServerSession(authOptions);
-    return (
-      <html lang="en">
-        <NextAuthProvider session={session}>
-          <body>{children}</body>
-        </NextAuthProvider>
-      </html>
-    )
-  }
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await getServerSession(authOptions);
+  return (
+    <html lang="en">
+      <NextAuthProvider session={session}>
+        <body>
+          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-2">
+            <Link className="flex items-center flex-shrink-0 text-white mr-10" href="/">
+              <img src="/favicon.ico" className="h-10" alt="Programmers' diary logo" />
+              <span className="font-semibold text-xl tracking-tight">Programmers' diary</span>
+            </Link>
+          </nav>
+          {children}
+        </body>
+      </NextAuthProvider>
+    </html>
+  )
+}
