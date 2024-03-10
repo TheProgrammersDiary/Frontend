cace9db0fcbd73c2be009f57566dd759e9515d8a
Evaldas Visockas <vievaldas@gmail.com>
2024-03-10 14:34:51 +0200

    Revert "Add logging"
    This reverts commit 0da423d11a9afdc156e606a13f7e16b275b681a4.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 module.exports = {
     blogUrl: process.env.NEXT_PUBLIC_blog_url || "https://localhost:8080",
     postUrl: process.env.NEXT_PUBLIC_post_url || "https://localhost:8081",
-    logging: {
-        fetches: {
-          fullUrl: true,
-        },
-      },
 };
